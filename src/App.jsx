e98c5357c66db13e859ea67707d0c05f14e00d80
sahil07 <sahilkamila143@gmail.com>
2025-03-24 19:48:41 +0530

    Added contact page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import NotFound from './Pages/NotFound';
 import Signup from './Pages/Signup';
 import Login from './Pages/Login';
 import CourseList from './Pages/Course/CourseList';
+import Contact from './Pages/Contact';
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" element={<HomePage />} ></Route>
         <Route path="/about" element={<AboutUs />} ></Route>
         <Route path="/courses" element={<CourseList />} ></Route>
+        <Route path="/contact" element={<Contact />} ></Route>
 
         <Route path="/signup" element={<Signup />} ></Route>
         <Route path="/login" element={<Login />} ></Route>
